a413d7ae0d9c175f984c8d7d406df9f14c0dda2e
Arvydas <arvydas.simbelis@gmail.com>
2025-03-05 22:59:20 +0200

    Inital_commit

---
+++ openai-client-demo.py
@@ -0,0 +1,23 @@
+import os
+from openai import OpenAI
+
+from dotenv import load_dotenv
+from rich import print
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-4o",
+)
+
+print(chat_completion)
